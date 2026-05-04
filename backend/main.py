e17ea3291add7a3c5e64820b062b5665d8df742e
Miguel Ramírez <miguel.ramirez0238@alumnos.udg.mx>
2026-05-04 00:12:30 -0600

    actualice la consulta de reportes de sql

--- backend/main.py
+++ backend/main.py
@@ -19,12 +19,34 @@ app.add_middleware(
 )
 
 DB_CONFIG = {
-    "dbname": "", #aqui ponen el nombre su base de datos, el que crearon en postgres 
+    "dbname": "cinemx_prototipo", #aqui ponen el nombre su base de datos, el que crearon en postgres 
     "user": "postgres",           
-    "password": "", # su contraseña locos
+    "password": "byemike24", # su contraseña locos
     "host": "localhost",
     "port": "5432"
 }
 
 def get_db_connection():
     return psycopg2.connect(**DB_CONFIG)
+
+# Recibe la consulta Sql, la ejecuta y devuelve la tabla
+@app.post("/api/consulta-libre")
+def ejecutar_consulta_libre(consulta: ConsultaSQL):
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(consulta.query)
+        if cursor.description:
+            resultados = cursor.fetchall()
+            if not resultados:
+                return [{"Mensaje": "La consulta se ejecutó, pero no se encontraron registros."}]
+        else:
+            conn.commit()
+            resultados = [{"Mensaje": "Consulta ejecutada con éxito (Sin datos para mostrar)"}]
+            
+        cursor.close()
+        conn.close()
+        return resultados
+        
+    except Exception as e:
+        return [{"error_sql": str(e)}]
